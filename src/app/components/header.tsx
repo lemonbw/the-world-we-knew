f445e89a56f93882dae59382661fa0d4698eb67e
lemonbw <leonblackwhite8@gmail.com>
2026-01-06 21:53:47 +0500

    feat(header): create header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="h-[10vh] w-full flex items-center justify-center bg-[#010407] border-b border-white">
+      <h1 className="text-white text-xl font-bold">The World We Knew</h1>
+    </div >
+  )
+}
