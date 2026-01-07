0fafa13a1948a81520f6509b0bb03513915016f2
lemonbw <leonblackwhite8@gmail.com>
2026-01-07 20:01:46 +0500

    refactor(header): replace buttons with links

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,13 +6,13 @@ export default function Header() {
       </div>
       <div className="flex justify-between mt-4 w-full mb-4">
         <div className="flex gap-24 ml-45 lg:text-[1.5rem]">
-          <button className="px-2 py-[1px] text-white rounded border">Главная</button>
-          <button className="px-2 py-[1px] text-white rounded border">Архив</button>
+          <a href="" className="px-2 py-[1px] text-white rounded border">Главная</a>
+          <a href="" className="px-2 py-[1px] text-white rounded border">Архив</a>
         </div>
 
         <div className="flex gap-24 mr-45 lg:text-[1.5rem]">
-          <button className="px-2 py-[1px] text-white rounded border">Карта</button>
-          <button className="px-2 py-[1px] text-white rounded border">Новости</button>
+          <a href="" className="px-2 py-[1px] text-white rounded border">Карта</a>
+          <a href="" className="px-2 py-[1px] text-white rounded border">Новости</a>
         </div>
       </div>
     </header>
