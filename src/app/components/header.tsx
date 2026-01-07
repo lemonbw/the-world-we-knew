2a99bcca68bba23d8fc9e39f36db44e67bbbabb3
lemonbw <leonblackwhite8@gmail.com>
2026-01-07 18:29:25 +0500

    feat(header): create navigation buttons

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,21 @@
 export default function Header() {
   return (
-    <div className="h-[10vh] w-full flex items-center justify-center bg-[#010407] border-b border-white">
-      <h1 className="text-white text-xl font-bold">The World We Knew</h1>
-    </div >
+    <header className="w-full bg-[#010407] border-b border-white">
+      <div className="h-[5vh] mt-6 flex items-center justify-center">
+        <h1 className="text-white lg:text-3xl font-bold">The World We Knew</h1>
+      </div>
+      <div className="flex justify-between mt-4 w-full mb-4">
+        <div className="flex gap-24 ml-45 lg:text-[1.5rem]">
+          <button className="px-2 py-[1px] text-white rounded border">Главная</button>
+          <button className="px-2 py-[1px] text-white rounded border">Архив</button>
+        </div>
+
+        <div className="flex gap-24 mr-45 lg:text-[1.5rem]">
+          <button className="px-2 py-[1px] text-white rounded border">Карта</button>
+          <button className="px-2 py-[1px] text-white rounded border">Новости</button>
+        </div>
+      </div>
+    </header>
+
   )
 }
