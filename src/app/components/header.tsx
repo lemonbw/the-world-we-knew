626d779d4e6976020efd3c6a0fbfc88acec84291
lemonbw <leonblackwhite8@gmail.com>
2026-01-08 11:49:23 +0500

    feat(navigation): make site title link to homepage

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="w-full bg-[#010407] border-b border-white">
       <div className="h-[5vh] mt-4 flex items-center justify-center">
-        <h1 className="text-white lg:text-3xl font-bold">The World We Knew</h1>
+        <Link href="/"><h1 className="text-white lg:text-3xl font-bold">The World We Knew</h1></Link>
       </div>
       <div className="flex justify-between mt-3 w-full mb-4">
         <div className="flex gap-24 ml-45 lg:text-[1.5rem]">
