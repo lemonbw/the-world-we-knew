61a7bef80dcc122318a8ba68df710ff9a1b3a9ae
lemonbw <leonblackwhite8@gmail.com>
2026-01-14 19:52:48 +0500

    feat(chapters): add sorting logic and toggle button

--- src/app/components/ChapterList/ChapterList.tsx
+++ src/app/components/ChapterList/ChapterList.tsx
@@ -1,36 +1,30 @@
 "use client";
 import { useState } from "react";
+import { useMemo } from "react";
 import Link from "next/link";
 import ChapterSort from "./ChapterSort"
 
 export default function ChapterList() {
 
-  const [hoveredIndex, setHovered] = useState(-1);
+  const [hoveredIndex, setHovered] = useState(-2);
 
-  const chapters = [
-    {
-      volume: "1",
-      chapter: "1",
-      index: 0,
-      title: "Начало конца",
-      symbols: "5К",
-      date: "2026.01.09",
-      href: "/chapter/1",
-    },
-    {
-      volume: "1",
-      chapter: "2",
-      index: 1,
-      title: "Обыкновенные деньки",
-      symbols: "5К",
-      date: "2026.01.09",
-      href: "/chapter/2",
-    },
-  ];
+  const [isAsc, setIsAsc] = useState<"asc" | "desc">("asc");
+
+  const NO_HOVER: number = -2;
+  const BUTTON_HOVER: number = -1;
+
+  const toggleOrder = () => setIsAsc(prev => (prev === "asc" ? "desc" : "asc"));
+
+
+  const sortedChapters = useMemo(() => ChapterSort(isAsc), [isAsc]);
 
   return (
     <section className="w-[80vw] mx-auto mt-4">
-      <ChapterSort></ChapterSort>
+      <button className="relative block mx-auto font-bold cursor-pointer" onMouseEnter={() => setHovered(BUTTON_HOVER)
+      } onMouseLeave={() => setHovered(NO_HOVER)} onClick={toggleOrder} >
+        <span className="">Сортировка ⇧⇩</span>
+        <span className={`absolute inline-block left-0 bottom-0 h-[2px] bg-white origin-left transition-all duration-300`} style={{ width: hoveredIndex === -1 ? "100%" : "0%" }}></span>
+      </button >
       <table className="w-full border-collapse">
         <thead>
           <tr className="bg-gray-200 dark:bg-black text-gray-800 dark:text-gray-200">
@@ -42,12 +36,12 @@ export default function ChapterList() {
           </tr>
         </thead>
         <tbody>
-          {chapters.map((c) => (
+          {sortedChapters.map((c) => (
             <tr
               key={c.href}
-              className="bg-black border-b transmition-colors duration-1000 cursor-pointer hover:text-black"
+              className="bg-black border-b transition-colors duration-1000 cursor-pointer hover:text-black"
               onMouseEnter={() => setHovered(c.index)}
-              onMouseLeave={() => setHovered(-1)}>
+              onMouseLeave={() => setHovered(NO_HOVER)}>
               <td className="px-4 py-2 relative z-10">
                 <Link href={c.href} className="block w-full h-full z-10">{c.volume}</Link>
                 <span
---
+++ src/app/components/ChapterList/ChapterSort.ts
@@ -0,0 +1,9 @@
+"use client";
+import { chapters } from "./chapters";
+import { Chapter } from "./chapterTypes";
+
+export default function ChapterSort(order: "asc" | "desc"): Chapter[] {
+  return [...chapters].sort((a, b) =>
+    order === "asc" ? a.index - b.index : b.index - a.index,
+  );
+}
