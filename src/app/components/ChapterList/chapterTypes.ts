3df6c0637abfbf360669e41f0466336f5d4825c9
lemonbw <leonblackwhite8@gmail.com>
2026-01-14 19:52:48 +0500

    feat(chapters): add sorting logic and toggle button

---
+++ src/app/components/ChapterList/chapterTypes.ts
@@ -0,0 +1,10 @@
+export interface Chapter {
+  volume: string;
+  chapter: string;
+  index: number;
+  title: string;
+  symbols: string;
+  date: string;
+  href: string;
+}
+
---
+++ src/app/components/ChapterList/chapters.tsx
@@ -0,0 +1,21 @@
+export const chapters = [
+  {
+    volume: "1",
+    chapter: "1",
+    index: 0,
+    title: "Начало конца",
+    symbols: "5К",
+    date: "2026.01.09",
+    href: "/chapter/1",
+  },
+  {
+    volume: "1",
+    chapter: "2",
+    index: 1,
+    title: "Обыкновенные деньки",
+    symbols: "5К",
+    date: "2026.01.09",
+    href: "/chapter/2",
+  },
+];
+
