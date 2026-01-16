6ccb86872f20a1106e421ba7e63f47cd6a6fe6ae
lemonbw <leonblackwhite8@gmail.com>
2026-01-16 20:49:31 +0500

    feat(ChapterList): add fade-in-out animation

--- src/app/components/ChapterList/ChapterList.tsx
+++ src/app/components/ChapterList/ChapterList.tsx
@@ -10,11 +10,20 @@ export default function ChapterList() {
 
   const [isAsc, setIsAsc] = useState<"asc" | "desc">("asc");
 
+  const [isPressed, setIsPressed] = useState(false);
+
   const NO_HOVER: number = -2;
   const BUTTON_HOVER: number = -1;
 
   const toggleOrder = () => setIsAsc(prev => (prev === "asc" ? "desc" : "asc"));
 
+  const activateSortButton = () => {
+    setIsPressed(true);
+    setTimeout(() => {
+      toggleOrder();
+      setIsPressed(false)
+    }, 1000)
+  }
 
   const sortedChapters = useMemo(() => ChapterSort(isAsc), [isAsc]);
 
@@ -23,7 +32,7 @@ export default function ChapterList() {
   return (
     <section className="w-[80vw] mx-auto mt-4">
       <button className="relative block mx-auto font-bold cursor-pointer" onMouseEnter={() => setHovered(BUTTON_HOVER)
-      } onMouseLeave={() => setHovered(NO_HOVER)} onClick={toggleOrder} >
+      } onMouseLeave={() => setHovered(NO_HOVER)} onClick={activateSortButton} >
         <span>Сортировка {arrows}</span>
         <span className={`absolute inline-block left-0 bottom-0 h-[2px] bg-white origin-left transition-all duration-300`} style={{ width: hoveredIndex === -1 ? "100%" : "0%" }}></span>
       </button >
@@ -45,22 +54,27 @@ export default function ChapterList() {
               onMouseEnter={() => setHovered(c.index)}
               onMouseLeave={() => setHovered(NO_HOVER)}>
               <td className="px-4 py-2 relative z-10">
-                <Link href={c.href} className="block w-full h-full z-10">{c.volume}</Link>
+                <Link href={c.href} className="block w-full h-full z-10 transition-colors duration-1000"
+                  style={{ color: isPressed ? "black" : "white" }}>{c.volume}</Link>
                 <span
                   className="absolute left-0 bottom-0 h-full bg-white origin-left transition-all duration-1000 -z-10"
                   style={{ width: hoveredIndex === c.index ? "80vw" : "0%" }}></span>
               </td>
               <td className="px-4 py-2 relative z-10">
-                <Link href={c.href} className="block w-full h-full">{c.chapter}</Link>
+                <Link href={c.href} className="block w-full h-full transition-colors duration-1000"
+                  style={{ color: isPressed ? "black" : "white" }}>{c.chapter}</Link>
               </td>
               <td className="px-4 py-2 relative z-10">
-                <Link href={c.href} className="block w-full h-full">{c.title}</Link>
+                <Link href={c.href} className="block w-full h-full transition-colors duration-1000"
+                  style={{ color: isPressed ? "black" : "white" }}>{c.title}</Link>
               </td>
               <td className="px-4 py-2 relative z-10">
-                <Link href={c.href} className="block w-full h-full">{c.symbols}</Link>
+                <Link href={c.href} className="block w-full h-full transition-colors duration-1000"
+                  style={{ color: isPressed ? "black" : "white" }}>{c.symbols}</Link>
               </td>
               <td className="px-4 py-2 relative z-10">
-                <Link href={c.href} className="block w-full h-full">{c.date}</Link>
+                <Link href={c.href} className="block w-full h-full transition-colors duration-1000"
+                  style={{ color: isPressed ? "black" : "white" }}>{c.date}</Link>
               </td>
             </tr>
           ))}
