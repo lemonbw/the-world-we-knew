68adf1854f3085f6985e35b39e1441d2e7c00255
lemonbw <leonblackwhite8@gmail.com>
2026-01-18 19:22:37 +0500

    fix(ChapterList): add smoothness to sorting animation, change size of text in table and add animation for arrow

--- src/app/components/ChapterList/ChapterList.tsx
+++ src/app/components/ChapterList/ChapterList.tsx
@@ -21,24 +21,28 @@ export default function ChapterList() {
     setIsPressed(true);
     setTimeout(() => {
       toggleOrder();
-      setIsPressed(false)
+    }, 800)
+    setTimeout(() => {
+      setIsPressed(false);
     }, 1000)
   }
 
   const sortedChapters = useMemo(() => ChapterSort(isAsc), [isAsc]);
 
-  const arrows = isAsc === "asc" ? "▼" : "▲"
+  //  const arrows = isAsc === "asc" ? "▼" : "▲"
 
   return (
     <section className="w-[80vw] mx-auto mt-4">
       <button className="relative block mx-auto font-bold cursor-pointer" onMouseEnter={() => setHovered(BUTTON_HOVER)
       } onMouseLeave={() => setHovered(NO_HOVER)} onClick={activateSortButton} >
-        <span>Сортировка {arrows}</span>
-        <span className={`absolute inline-block left-0 bottom-0 h-[2px] bg-white origin-left transition-all duration-300`} style={{ width: hoveredIndex === -1 ? "100%" : "0%" }}></span>
+        <span className="text-2xl -ml-2">Сортировка
+          <span className={`absolute inline-block transition-transform duration-800 ml-1 bottom-[-0.45rem] text-[2rem] ${isAsc === "asc" ? "rotate-0" : "-rotate-180"}`}>▼</span>
+        </span>
+        <span className={`absolute inline-block -left-1.5 bottom-0 h-[2px] bg-white origin-left transition-all duration-300`} style={{ width: hoveredIndex === -1 ? "120%" : "0%" }}></span>
       </button >
-      <table className="w-full border-collapse">
+      <table className="w-full border-collapse text-[1.2rem]">
         <thead>
-          <tr className="bg-gray-200 dark:bg-black text-gray-800 dark:text-gray-200">
+          <tr className="bg-gray-200 dark:bg-black text-gray-800 dark:text-gray-200 text-[1.3rem]">
             <th className="px-4 py-2 text-left">Том</th>
             <th className="px-4 py-2 text-left">Глава</th>
             <th className="px-4 py-2 text-left">Название</th>
@@ -54,10 +58,10 @@ export default function ChapterList() {
               onMouseEnter={() => setHovered(c.index)}
               onMouseLeave={() => setHovered(NO_HOVER)}>
               <td className="px-4 py-2 relative z-10">
-                <Link href={c.href} className="block w-full h-full z-10 transition-colors duration-1000"
+                <Link href={c.href} className="block w-full h-full z-20 transition-colors duration-1000"
                   style={{ color: isPressed ? "black" : "white" }}>{c.volume}</Link>
                 <span
-                  className="absolute left-0 bottom-0 h-full bg-white origin-left transition-all duration-1000 -z-10"
+                  className="absolute left-0 bottom-0 h-full bg-white origin-left transition-all duration-1000 -z-20"
                   style={{ width: hoveredIndex === c.index ? "80vw" : "0%" }}></span>
               </td>
               <td className="px-4 py-2 relative z-10">
