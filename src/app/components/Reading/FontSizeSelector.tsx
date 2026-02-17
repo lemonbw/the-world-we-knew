b338ad1eba475d094aa7fe9e26eea096d6a7dec0
lemonbw <leonblackwhite8@gmail.com>
2026-02-17 18:59:51 +0500

    feat(font-size): add min/max limits and expand dropdown values

--- src/app/components/Reading/FontSizeSelector.tsx
+++ src/app/components/Reading/FontSizeSelector.tsx
@@ -7,7 +7,7 @@ type FontSizeSelectorProps = {
 
 export default function FontSizeSelector({ currentSize, setCurrentSize }: FontSizeSelectorProps) {
 
-  const fontSizes = [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 32, 34, 36, 38, 40];
+  const fontSizes = [8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 32, 34, 36, 38, 40, 45, 50, 55, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120];
 
   const [isHidden, setIsHidden] = useState(true);
 
@@ -23,8 +23,8 @@ export default function FontSizeSelector({ currentSize, setCurrentSize }: FontSi
         const result = Function(`"use strict"; return (${filteredSize})`)();
 
         if (typeof result === "number" && isFinite(result)) {
-          setCurrentSize(result);
-          setTempSize(String(result));
+          setCurrentSize(result > 120 ? 120 : result < 8 ? 8 : result);
+          setTempSize(String(result > 120 ? 120 : result < 8 ? 8 : result));
         }
       } catch {
         console.log("Something wrong")
@@ -34,20 +34,21 @@ export default function FontSizeSelector({ currentSize, setCurrentSize }: FontSi
 
   return (
 
-    <div className="relative w-25 inline-block px-2 py-1 mr-1 h-9 mt-[1px]">
+    <div className="relative w-21 inline-block px-2 py-1 mr-1 h-9 mt-[1px]">
       <div className="flex border-1 rounded-sm">
         <input type="text" value={tempSize}
           onChange={(e) => setTempSize(e.target.value)}
-          onKeyDown={handleKeyDown} className="w-12 p-1 outline-none! rounded-l-sm focus:shadow-[inset_0_0_0_1.5px_theme(colors.white)]" />       <button
-            type="button"
-            onClick={() => setIsHidden(!isHidden)}
-            className="material-icons text-lg w-8 border-l"
-          >
+          onKeyDown={handleKeyDown} className="w-10 p-1 outline-none! rounded-l-sm focus:shadow-[inset_0_0_0_1.5px_theme(colors.white)]" />
+        <button
+          type="button"
+          onClick={() => setIsHidden(!isHidden)}
+          className="material-icons text-lg w-6 border-l"
+        >
           keyboard_arrow_down
         </button>
       </div>
       <div
-        className={`absolute h-48 overflow-y-auto border-1 left-0 top-10 w-21 ml-2 bg-black text-white flex flex-col z-50 rounded shadow ${isHidden ? "hidden" : ""
+        className={`absolute h-48 overflow-y-auto border-1 left-0 top-10 w-17 ml-2 bg-black text-white flex flex-col z-50 rounded shadow ${isHidden ? "hidden" : ""
           }`}
       >
         {fontSizes.map((size) => (
