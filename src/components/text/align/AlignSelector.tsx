a675dcf3035363c942f1191778f47b8d8e8bef87
lemonbw <leonblackwhite8@gmail.com>
2026-02-27 19:07:23 +0500

    fix(AlignSelector): fix width

--- src/components/text/align/AlignSelector.tsx
+++ src/components/text/align/AlignSelector.tsx
@@ -5,7 +5,7 @@ type AlignSelectorProps = {
 
 export function AlignSelector({ currentAlign, setCurrentAlign }: AlignSelectorProps) {
   return (
-    <div className="flex gap-1 *:text-[1.7rem]! mt-1 *:hover:border-1 *:border-white/10 *:rounded-sm *:duration-300 *:w-8 w-50">
+    <div className="flex gap-1 *:text-[1.7rem]! mt-1 *:border-1 *:border-black *:hover:border-white/10 *:rounded-sm *:duration-300 *:w-10 w-30">
       <button className={`material-icons ${currentAlign === "left" ? "bg-white/10" : ""}`} onClick={() => setCurrentAlign("left")}>format_align_left</button>
       <button className={`material-icons ${currentAlign === "center" ? "bg-white/10" : ""}`} onClick={() => setCurrentAlign("center")}>format_align_center</button>
       <button className={`material-icons ${currentAlign === "right" ? "bg-white/10" : ""}`} onClick={() => setCurrentAlign("right")}>format_align_right</button>
