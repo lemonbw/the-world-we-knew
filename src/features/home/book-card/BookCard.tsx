6b92857ca5a2d129a5230e7ec1901547deb08b57
lemonbw <leonblackwhite8@gmail.com>
2026-02-28 22:39:52 +0500

    refactor(rating): remove Rating from BookCard

--- src/features/home/book-card/BookCard.tsx
+++ src/features/home/book-card/BookCard.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import Rating from "@/src/features/home/book-card/Rating"
 import bookCover from "@/src/shared/assets/images/book-card/book-cover.jpg"
 
 export default function BookCard() {
@@ -10,7 +9,6 @@ export default function BookCard() {
         <div className="relative w-64 h-80 border-2 border-white">
           <Image src={bookCover} alt="book cover" fill className="object-cover" />
         </div>
-        <Rating></Rating>
       </div>
     </article>
   )
