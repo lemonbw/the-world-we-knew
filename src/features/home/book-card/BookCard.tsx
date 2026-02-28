92fa00b495c95666ee18aed6c6ac68861f1a4bcb
lemonbw <leonblackwhite8@gmail.com>
2026-02-28 22:55:56 +0500

    style(BookCard): add centering

--- src/features/home/book-card/BookCard.tsx
+++ src/features/home/book-card/BookCard.tsx
@@ -5,7 +5,7 @@ export default function BookCard() {
 
   return (
     <article className="flex justify-center mt-8">
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center items-center text-center">
         <div className="relative w-64 h-80 border-2 border-white">
           <Image src={bookCover} alt="book cover" fill className="object-cover" />
         </div>
