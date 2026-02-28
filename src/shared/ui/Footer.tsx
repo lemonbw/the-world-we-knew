1280c19d2ed6b139d3c066cbdf30219692da706c
lemonbw <leonblackwhite8@gmail.com>
2026-02-28 21:54:46 +0500

    style(footer): change social links position

--- src/shared/ui/Footer.tsx
+++ src/shared/ui/Footer.tsx
@@ -1,12 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="w-full flex items-start justify-center h-21 *:mt-2">
+    <footer className="w-full flex items-center justify-center h-21 *:mt-2">
       <p className="mr-4">© {new Date().getFullYear()} The World We Knew </p>
-      <p className="font-bold mr-2">Social:</p>
-      <ul className="flex flex-col flex-end text-left">
-        <li><a href="mailto:leonblackwhite@proton.me">Mail</a></li>
-        <li><a href="https://vk.com/theworldweknew">VK сообщество</a></li>
-        <li><a href="https://www.youtube.com/@LeonBlackWhiteMusic">Youtube</a></li>
-      </ul>
+      <div className="flexflex-col">
+        <p className="font-bold -mb-1 text-center">Social</p>
+        <ul className="flex gap-2 text-center">
+          <li><a href="mailto:leonblackwhite@proton.me">Mail</a></li>
+          <li><a href="https://vk.com/theworldweknew">VK сообщество</a></li>
+          <li><a href="https://www.youtube.com/@LeonBlackWhiteMusic">Youtube</a></li>
+        </ul>
+      </div>
     </footer>)
 }
