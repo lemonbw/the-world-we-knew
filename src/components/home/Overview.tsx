1b631359e76d3759e2e53394059eb8ffafb22354
lemonbw <leonblackwhite8@gmail.com>
2026-03-01 19:13:37 +0500

    feat(home): updato section toggle

---
+++ src/components/home/Overview.tsx
@@ -0,0 +1,7 @@
+export default function Description() {
+  return (
+    <div>
+      <p className="max-w-[60ch]" > Lorem ipsum dolor sit amet, consectetur adipisicing elit.Laudantium aspernatur asperiores dolore quaerat sequi.Repellendus quae quod eaque, voluptatem necessitatibus nulla in alias eius maxime blanditiis a optio nam nihil ?</p >
+    </div >
+  )
+}
--- src/components/home/Panel.tsx
+++ src/components/home/Panel.tsx
@@ -1,14 +1,26 @@
-type PanelProps = {
-  section: string;
-  setSection: (value: "Main" | "Chapters") => void;
-}
+"use client"
 
-export default function Panel({ section, setSection }: PanelProps) {
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export default function Panel() {
+  const pathname = usePathname();
+  const section = pathname === "/chapters" ? "Chapters" : "Overview";
 
   return (
     <div className="flex relative border rounded-xl w-60 h-10 mt-7 overflow-hidden *:duration-500">
-      <button className={`flex-1 text-2xl ${section === "Main" ? "bg-white text-black" : "bg-black text-white"}`} onClick={() => setSection("Main")}>Главная</button>
-      <button className={`flex-1 text-2xl ${section === "Chapters" ? "bg-white text-black" : "bg-black text-white"}`} onClick={() => setSection("Chapters")}>Главы</button>
+      <Link
+        href="/"
+        className={`flex-1 text-2xl ${section === "Overview" ? "bg-white text-black" : "bg-black text-white"}`}
+      >
+        Описание
+      </Link>
+      <Link
+        href="/chapters"
+        className={`flex-1 text-2xl ${section === "Chapters" ? "bg-white text-black" : "bg-black text-white"}`}
+      >
+        Главы
+      </Link>
 
       <div className="absolute left-1/2 h-full w-[1.5px] bg-white"></div>
     </div >
