ea68c51e06c002969c0466f2ab51c454a7662a0a
lemonbw <leonblackwhite8@gmail.com>
2026-03-11 17:24:51 +0500

    feat(panel): add mobile adaptation

--- src/components/home/Panel.tsx
+++ src/components/home/Panel.tsx
@@ -8,16 +8,16 @@ export default function Panel() {
   const section = pathname === "/chapters" ? "Chapters" : "Overview";
 
   return (
-    <div className="flex border rounded-xl w-55 h-10 mt-7 overflow-hidden *:w-1/2 *:pt-0.5 *:pl-4 *:text-center">
+    <div className="flex border rounded-[0.55rem] lg:rounded-xl w-40 h-9 lg:w-55 lg:h-10 mt-7 overflow-hidden *:w-1/2 *:pt-[0.2rem] *:pl-[0.5rem] lg:*:pl-4 *:text-center">
       <Link
         href="/"
-        className={`flex text-2xl ${section === "Overview" ? "bg-white text-black" : "bg-black text-white"}`}
+        className={`flex text-xl lg:text-2xl ${section === "Overview" ? "bg-white text-black" : "bg-black text-white"}`}
       >
         Обзор
       </Link>
       <Link
         href="/chapters"
-        className={`flex text-2xl ${section === "Chapters" ? "bg-white text-black" : "bg-black text-white"}`}
+        className={`flex text-xl lg:text-2xl ${section === "Chapters" ? "bg-white text-black" : "bg-black text-white"}`}
       >
         Главы
       </Link>
