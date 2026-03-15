ac04d13cf235de67d31ba35c406137ce996612bc
lemonbw <leonblackwhite8@gmail.com>
2026-03-15 16:15:07 +0500

    style(footer): add mobile styles

--- src/shared/ui/Footer.tsx
+++ src/shared/ui/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="w-full flex items-center justify-center h-21 *:mt-2">
-      <p className="mr-4">© {new Date().getFullYear()} The World We Knew </p>
-      <div className="flexflex-col">
-        <p className="font-bold -mb-1 text-center">Social</p>
+    <footer className="w-full flex items-center justify-center h-21 text-xs lg:text-lg *:mt-2">
+      <p className="flex flex-row flex-wrap w-15 lg:w-60 mr-4">© {new Date().getFullYear()} The World We Knew </p>
+      <div className="flex flex-col">
+        <p className="font-bold lg:-mb-1 text-center">Social</p>
         <ul className="flex gap-2 text-center">
           <li><a href="mailto:leonblackwhite@proton.me">Mail</a></li>
           <li><a href="https://vk.com/theworldweknew">VK сообщество</a></li>
