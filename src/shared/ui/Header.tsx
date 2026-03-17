16648dd34a136fe09114479289caee09412887d1
lemonbw <leonblackwhite8@gmail.com>
2026-03-17 16:41:24 +0500

    feat(navigation): show navigation when hamberger clicked

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link';
 export default function Header() {
   const [hoveredIndex, setHovered] = useState(0);
 
-  const [scrolled, setScrolled] = useState(false);
+  const [hiddenHeader, setHiddenHeader] = useState(false);
 
-  const [hidden, setHidden] = useState(false);
+  const [hiddenNavigation, setHiddenNavigation] = useState(true);
 
   const prevScrollY = useRef(0);
 
@@ -16,7 +16,7 @@ export default function Header() {
       const currentScroll = window.scrollY;
       const previousScroll = prevScrollY.current;
 
-      setHidden(currentScroll > previousScroll);
+      setHiddenHeader(currentScroll > previousScroll);
 
       prevScrollY.current = currentScroll;
     };
@@ -26,21 +26,11 @@ export default function Header() {
     return () => window.removeEventListener('scroll', handleScrollPos);
   }, []);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 0);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  const stickClassName = `relative block h-1 ${scrolled ? 'bg-black' : 'bg-white'}`;
+  const stickClassName = `relative block w-8 h-1.5 bg-white`;
 
   return (
     <header
-      className={`fixed z-10 flex h-[3.5rem] w-full items-center justify-between border-white bg-black px-4 duration-500 lg:static lg:mt-5 lg:block lg:h-[15vh] lg:bg-black lg:text-white ${scrolled ? 'bg-white text-black' : 'bg-black text-white'} ${hidden ? '-translate-y-full' : 'translate-y-0'} lg:translate-y-0`}
+      className={`fixed z-10 flex h-[3.5rem] w-full items-center justify-between border-white bg-black px-2 duration-300 lg:duration-500 lg:static lg:mt-5 lg:block lg:h-[15vh] lg:bg-black lg:text-white ${hiddenHeader ? '-translate-y-full' : 'translate-y-0'} lg:translate-y-0`}
     >
       <div className="top-0 left-2 z-10 flex h-[2.5rem] items-center justify-center lg:mt-4">
         <Link
@@ -53,46 +43,46 @@ export default function Header() {
             The World We Knew
           </h1>
           <span
-            className="absolute bottom-0 left-0 h-[2px] origin-left bg-white transition-all duration-300"
+            className="absolute bottom-0 left-0 h-[2px] origin-left bg-white transition-all duration-300 hidden lg:inline"
             style={{ width: hoveredIndex === 5 ? '102%' : '0%' }}
           ></span>
         </Link>
       </div>
-      <button className="z-10 flex h-5 w-6 flex-col justify-between lg:hidden">
+      <button className="z-10 flex h-7 w-10 flex-col justify-between lg:hidden" onClick={() => setHiddenNavigation(!hiddenNavigation)}>
         <span className={stickClassName}></span>
         <span className={stickClassName}></span>
         <span className={stickClassName}></span>
       </button>
-      <nav className="mt-3 mb-4 hidden w-full justify-between lg:flex">
-        <div className="ml-45 flex gap-24 lg:text-[1.7rem]">
+      <nav className={`z-5 absolute lg:static flex-col lg:flex lg:flex-row lg:translate-x-0 justify-between ${hiddenNavigation ? "translate-x-full" : "translate-x-0"} duration-300 w-full mt-3 mb-4 -ml-2 pl-2 lg:ml-0 lg:pl-0 text-[1.3rem] font-medium ${hiddenHeader ? "translate-x-full" : "translate-x-0"} bg-black`}>
+        <div className="lg:ml-45 block lg:flex gap-24 lg:text-[1.7rem]">
           <Link
             href="/"
-            className="relative"
+            className="relative block mt-40 lg:mt-0"
             onMouseEnter={() => setHovered(1)}
             onMouseLeave={() => setHovered(0)}
           >
             <span>Главная</span>
             <span
-              className="absolute bottom-0 left-0 h-[2px] origin-left bg-white transition-all duration-300"
+              className="absolute bottom-0 left-0 h-[2px] origin-left bg-white transition-all duration-300 hidden lg:inline"
               style={{ width: hoveredIndex === 1 ? '102%' : '0%' }}
             ></span>
           </Link>
 
           <Link
             href="/archive"
-            className="relative"
+            className="relative block"
             onMouseEnter={() => setHovered(2)}
             onMouseLeave={() => setHovered(0)}
           >
             <span>Архив</span>
             <span
-              className="absolute bottom-0 -left-0.5 h-[2px] origin-left bg-white transition-all duration-300"
+              className="absolute bottom-0 -left-0.5 h-[2px] origin-left bg-white transition-all duration-300 hidden lg:inline"
               style={{ width: hoveredIndex === 2 ? '102%' : '0%' }}
             ></span>
           </Link>
         </div>
 
-        <div className="mr-45 flex gap-24 lg:text-[1.7rem]">
+        <div className="mr-45 block lg:flex gap-24 lg:text-[1.7rem]">
           <Link
             href="/map"
             className="relative"
@@ -101,20 +91,20 @@ export default function Header() {
           >
             <span>Карта</span>
             <span
-              className="absolute bottom-0 left-[1px] h-[2px] origin-left bg-white transition-all duration-300"
+              className="absolute bottom-0 left-[1px] h-[2px] origin-left bg-white transition-all duration-300 hidden lg:inline"
               style={{ width: hoveredIndex === 3 ? '103%' : '0%' }}
             ></span>
           </Link>
 
           <Link
             href="/news"
-            className="relative"
+            className="relative block"
             onMouseEnter={() => setHovered(4)}
             onMouseLeave={() => setHovered(0)}
           >
             <span>Новости</span>
             <span
-              className="absolute bottom-0 left-[1px] h-[2px] origin-left bg-white transition-all duration-300"
+              className="absolute bottom-0 left-[1px] h-[2px] origin-left bg-white transition-all duration-300 hidden lg:inline"
               style={{ width: hoveredIndex === 4 ? '102%' : '0%' }}
             ></span>
           </Link>
