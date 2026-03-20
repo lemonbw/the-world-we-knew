b096d11324e1a2790679e0e3ffed1a05fb3eb321
lemonbw <leonblackwhite8@gmail.com>
2026-03-20 16:40:04 +0500

    style(panel): decrease panel size for mobile devices

--- src/components/home/Panel.tsx
+++ src/components/home/Panel.tsx
@@ -8,16 +8,16 @@ export default function Panel() {
   const section = pathname === '/chapters' ? 'Chapters' : 'Overview';
 
   return (
-    <div className="mt-7 flex h-9 w-40 overflow-hidden rounded-[0.55rem] border *:w-1/2 *:pt-[0.2rem] *:pl-[0.5rem] *:text-center lg:h-10 lg:w-55 lg:rounded-xl lg:*:pl-4">
+    <div className="mt-7 flex h-9 w-35 *:text-[1.2rem] overflow-hidden rounded-[0.55rem] border *:w-1/2 *:pt-[0.2rem] *:pl-[0.3rem] *:text-center lg:h-10 lg:w-55 lg:rounded-xl lg:*:pl-4">
       <Link
         href="/"
-        className={`flex text-xl lg:text-2xl ${section === 'Overview' ? 'bg-white text-black' : 'bg-black text-white'}`}
+        className={`flex lg:text-2xl ${section === 'Overview' ? 'bg-white text-black' : 'bg-black text-white'}`}
       >
         Обзор
       </Link>
       <Link
         href="/chapters"
-        className={`flex text-xl lg:text-2xl ${section === 'Chapters' ? 'bg-white text-black' : 'bg-black text-white'}`}
+        className={`flex lg:text-2xl ${section === 'Chapters' ? 'bg-white text-black' : 'bg-black text-white'}`}
       >
         Главы
       </Link>
