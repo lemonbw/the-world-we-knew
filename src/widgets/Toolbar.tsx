5a73d063501f3242f8b48a1dcd57e21bbe5d9887
lemonbw <leonblackwhite8@gmail.com>
2026-03-27 18:22:30 +0500

    style(toolbar): dicrease toolbar width and top margin

--- src/widgets/Toolbar.tsx
+++ src/widgets/Toolbar.tsx
@@ -44,12 +44,12 @@ export function Toolbar({ ChapterSelector, children }: ToolbarProps) {
     <section
       ref={readingSection}
       id="reading-section"
-      className={`relative mx-auto mt-12 flex h-[100vh] w-[80vw] flex-col lg:h-[105vh]`}
+      className={`relative mx-auto mt-12 lg:mt-8 flex h-[100vh] w-[80vw] flex-col lg:h-[105vh]`}
     >
       <div
         className={`rotate-x-180 *:rotate-x-180 relative -mb-45 h-62 ${ChapterSelector ? "w-[120vw]" : "w-[100vw]"} overflow-x-auto ${fullscreen ? 'lg:mt-4' : ''}`}>
         <div
-          className={`absolute bottom-0 mb-2 -ml-[0.225rem] flex h-10 w-[195vw] lg:w-full flex-none items-center gap-1 lg:-ml-1`}
+          className={`absolute bottom-0 mb-2 -ml-[0.225rem] flex h-10 w-[168vw] lg:w-full flex-none items-center gap-1 lg:-ml-1`}
         >
           {ChapterSelector}
           <FontSelector
@@ -75,7 +75,7 @@ export function Toolbar({ ChapterSelector, children }: ToolbarProps) {
         </div>
       </div>
       <div
-        className={`flex-1 overflow-hidden rounded-2xl border ${fullscreen ? 'mb-4 lg:mb-0' : ''}`}
+        className={`flex-1 overflow-hidden border rounded-2xl -mt-4 ${fullscreen ? 'mb-4 lg:mb-0' : ''}`}
       >
         <div
           className="h-full overflow-y-auto p-1"
