6af14f7913a39c3ca51421adcd52bc1a5f6383ac
lemonbw <leonblackwhite8@gmail.com>
2026-03-27 19:11:27 +0500

    feat(book-card): change image based on theme

--- src/features/home/ui/BookCard.tsx
+++ src/features/home/ui/BookCard.tsx
@@ -1,13 +1,19 @@
+"use client"
 import Image from 'next/image';
-import bookCover from '@/src/shared/assets/images/book-card/book-cover.jpg';
+import { useMedia } from 'use-media';
+import darkBookCover from '@/src/shared/assets/images/book-card/dark-book-cover.jpg';
+import whiteBookCover from '@/src/shared/assets/images/book-card/white-book-cover.png'
 
 export default function BookCard() {
+
+  const isDark = useMedia({ 'prefers-color-scheme': 'dark' })
+
   return (
     <article className="mt-20 flex justify-center lg:mt-8">
       <div className="flex flex-col items-center justify-center text-center">
-        <div className="relative h-46 w-40 border-2 border-white lg:h-80 lg:w-64">
+        <div className="relative h-46 w-40 border-2 lg:h-80 lg:w-64">
           <Image
-            src={bookCover}
+            src={isDark ? darkBookCover : whiteBookCover}
             alt="book cover"
             fill
             className="object-cover"
