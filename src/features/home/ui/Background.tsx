71adc14f830471b0eea525ca32476042d4c15208
lemonbw <leonblackwhite8@gmail.com>
2026-04-07 21:50:00 +0500

    feat(book-card): add eyes

--- src/features/home/ui/Background.tsx
+++ src/features/home/ui/Background.tsx
@@ -1,6 +1,10 @@
 "use client"
 import Image from 'next/image';
 import { useMedia } from 'use-media';
+import blackEyelids from '@/src/shared/assets/images/book-card/black-eyelids.svg';
+import whiteEyelids from '@/src/shared/assets/images/book-card/white-eyelids.svg';
+import blackEyes from '@/src/shared/assets/images/book-card/black-eyes.svg';
+import whiteEyes from '@/src/shared/assets/images/book-card/white-eyes.svg';
 import blackChains from '@/src/shared/assets/images/book-card/black-broken-chains-part.svg';
 import whiteChains from '@/src/shared/assets/images/book-card/white-broken-chains-part.svg';
 
@@ -9,20 +13,34 @@ export default function Background() {
   const isDark = useMedia({ 'prefers-color-scheme': 'dark' })
 
   return (
-    <article className="relative mt-20 w-2000 h-40 flex justify-center lg:mt-8">
+    <article className="relative mt-20 -mb-25 w-2000 h-40 flex justify-center lg:mt-8">
+      <div className="relative h-[37px] w-65 overflow-hidden">
+        <Image
+          src={isDark ? whiteEyelids : blackEyelids}
+          alt="eyelids"
+          fill
+          className="object-cover mt-[2px] opacity-70 blur-[1px]"
+        />
+        <Image
+          src={isDark ? whiteEyes : blackEyes}
+          alt="eyes"
+          fill
+          className="object-cover mt-[3px] opacity-70 blur-[1px]"
+        />
+      </div>
       <Image
         src={isDark ? whiteChains : blackChains}
         alt="chains"
-        width={1370}
+        width={1500}
         height={750}
-        className="absolute z-0 -mt-10"
+        className="absolute z-0 -mt-10 opacity-90 blur-[0.5px]"
       />
       <Image
         src={isDark ? whiteChains : blackChains}
         alt="chains"
-        width={1370}
+        width={1500}
         height={750}
-        className="absolute rotate-180 mt-50"
+        className="absolute rotate-180 mt-20 opacity-90 blur-[0.5px]"
       />
     </article>
   );
