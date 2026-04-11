5d9b06fe0359128993357061f6da4d57affa8e77
lemonbw <leonblackwhite8@gmail.com>
2026-04-11 12:50:50 +0500

    refactor(panel): extract panel state into usePanelState hook

--- src/features/home/ui/Panel.tsx
+++ src/features/home/ui/Panel.tsx
@@ -1,11 +1,10 @@
 'use client';
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import usePanelState from '@/src/features/home/model/usePanelState';
 
 export default function Panel() {
-  const pathname = usePathname();
-  const section = pathname === '/chapters' ? 'Chapters' : 'Overview';
+
+  const section = usePanelState();
 
   return (
     <div className={`z-5 mt-30 ${section === 'Overview' ? "-mb-10 lg:-mb-6" : ""} flex h-9 w-35 *:text-[1.2rem] overflow-hidden rounded-[0.55rem] border-2 *:w-1/2 *:pt-[0.2rem] *:pl-[0.3rem] *:text-center lg:h-10 lg:w-55 lg:rounded-xl lg:*:pl-4`}>
