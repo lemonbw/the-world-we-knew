3b47b69169d258730582e0dec4b29d091c94a044
lemonbw <leonblackwhite8@gmail.com>
2026-08-10 18:08:16 +0500

    feat(views): introduce views layer for home, chapters, and reading pages

---
+++ src/views/chapters/ui/ChaptersPage.tsx
@@ -0,0 +1,9 @@
+import { ChapterList } from '@/src/widgets/chapter-list';
+
+export const ChaptersPage = () => {
+  return (
+    <main className="flex flex-col items-center justify-center overflow-x-hidden bg-white dark:bg-[#010407]">
+      <ChapterList />
+    </main>
+  );
+};
---
+++ src/views/reading/ui/ReadingPage.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { Toolbar } from '@/src/widgets/toolbar';
+import { MarkdownRenderer } from '@/src/shared/ui/MarkdownRenderer';
+import { ChapterSelector } from '@/src/features/chapter-selector';
+
+export const ReadingPage = ({ content }: { content: string }) => {
+  return (
+    <main className="flex flex-col items-center justify-center overflow-x-hidden bg-white dark:bg-[#010407]">
+      <Toolbar ChapterSelector={<ChapterSelector />}>
+        <MarkdownRenderer
+          className="h-[90vh] overflow-y-auto p-1"
+          content={content}
+        />
+      </Toolbar>
+    </main>
+  );
+};
