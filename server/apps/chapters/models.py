5803fb9ccb9ff54795bdd275514617e8669fdf67
lemonbw <leonblackwhite8@gmail.com>
2026-08-17 13:39:02 +0500

    refactor(chapters): update chapter model

--- server/apps/chapters/models.py
+++ server/apps/chapters/models.py
@@ -3,13 +3,16 @@ from django.db import models
 
 class Chapter(models.Model):
     volume = models.PositiveIntegerField()
-    chapter = models.PositiveIntegerField()
-    index = models.PositiveIntegerField()
+    chapter = models.DecimalField(
+        max_digits=5,
+        decimal_places=1,
+    )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
     symbols = models.PositiveIntegerField()
-    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
