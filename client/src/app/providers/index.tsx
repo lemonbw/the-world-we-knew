a1c697b7c4f7bcfde80540cb095ed172dafb7a8a
lemonbw <leonblackwhite8@gmail.com>
2026-08-22 13:29:18 +0500

    feat(scroll): integrate lenis for smooth scrolling

--- client/src/app/providers/index.tsx
+++ client/src/app/providers/index.tsx
@@ -1,11 +1,16 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { ReactLenis } from 'lenis/react';
 
 type Props = {
   children: ReactNode;
 };
 
 export const Providers = ({ children }: Props) => {
-  return <>{children}</>;
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.2, smoothWheel: true }}>
+      {children}
+    </ReactLenis>
+  );
 };
